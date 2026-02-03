612dd70685854899a438a8d59110952885e3c0a9
Epicarism <epicarism@icloud.com>
2026-02-03 01:06:45 +0100

    feat: Enable multilingual TTS with eSpeakNG
    - Enable eSpeakNGSwift dependency (was commented out)
    - Extend Language enum with European and Asian languages
    - Languages: Italian, Spanish, French, Portuguese, German, Japanese, Korean, Chinese, Hindi

--- Sources/KokoroSwift/TextProcessing/Language.swift
+++ Sources/KokoroSwift/TextProcessing/Language.swift
@@ -5,11 +5,36 @@ import Foundation
 
 /// Supported languages for text-to-speech synthesis.
 /// This enum defines the available language variants that can be used with the Kokoro TTS engine.
+/// Extended for multilingual Kokoro support with eSpeak-NG phonemization.
 public enum Language: String, CaseIterable {
   /// No language specified or language-independent processing.
   case none = ""
+  
+  // MARK: - English
   /// US English (American English).
   case enUS = "en-us"
   /// GB English (British English).
   case enGB = "en-gb"
+  
+  // MARK: - European Languages
+  /// Italian
+  case italian = "it"
+  /// Spanish
+  case spanish = "es"
+  /// French
+  case french = "fr-fr"
+  /// Portuguese (Brazilian)
+  case portuguese = "pt-br"
+  /// German
+  case german = "de"
+  
+  // MARK: - Asian Languages
+  /// Japanese
+  case japanese = "ja"
+  /// Korean
+  case korean = "ko"
+  /// Chinese (Mandarin)
+  case chinese = "cmn"
+  /// Hindi
+  case hindi = "hi"
 }
